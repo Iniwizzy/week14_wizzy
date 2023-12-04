6cdfb52c5d59e031b993f86b849f267841e44d40
Wizzy <Badawilovina@gmail.com>
2023-12-04 18:16:59 +0700

    commit pertama

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'WEEK13',
+    version = '0.1.0',
+    author = 'Wizzy',
+    author_email = 'wizzy@polines.ac.id',
+    description = 'Ini adalah sebuah tugas dasar pemrograman',
+    packages = find_packages(),
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        ],
+        python_requires='>=3.6',
+)
